4d953123f774d55149a525fb2cf50cb5ac00fca5
alexander134 <alexanderdiaz_134@hotmail.com>
2023-03-31 11:47:48 -0300

    CRUD en controlador y JWT desarrollados

--- src/main/java/com/sistema/logindos/servicio/UsuarioServicioIMP.java
+++ src/main/java/com/sistema/logindos/servicio/UsuarioServicioIMP.java
@@ -2,19 +2,9 @@ package com.sistema.logindos.servicio;
 
 import com.sistema.logindos.dto.RegistroUsuarioDTO;
 import com.sistema.logindos.dto.UsuarioRespuesta;
-import com.sistema.logindos.entidades.Usuario;
-import com.sistema.logindos.repositorios.UsuarioRepo;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class UsuarioServicioIMP implements UsuarioServicio{
 
-	@Autowired
-	private UsuarioRepo usuarioRepo;
-	
-	@Autowired
-	private ModelMapper modelmapper;
 	@Override
 	public UsuarioRespuesta getListUsuarios(int numeroPag, int cantRegistro, String orderPorCampo, String sortDir) {
 		// TODO Auto-generated method stub
@@ -37,16 +27,6 @@ public class UsuarioServicioIMP implements UsuarioServicio{
 	public void deleteUsuarioId(Long idUsuario) {
 		// TODO Auto-generated method stub
 		
-	}
-	
-	private RegistroUsuarioDTO mapENTITYtoDTO(Usuario usuario) {
-		RegistroUsuarioDTO usuarioDTO = modelmapper.map(usuario, RegistroUsuarioDTO.class);
-		return usuarioDTO;
-	}
-	
-	private Usuario mapDTOtoENTITY(RegistroUsuarioDTO usuarioDTO) {
-		Usuario usuario = modelmapper.map(usuarioDTO, Usuario.class);
-		return usuario;
 	}
 
 }
